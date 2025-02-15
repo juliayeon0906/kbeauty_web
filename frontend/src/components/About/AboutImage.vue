db9b8bfce5cd159fcffe968dffbebbbb1605b853
Najuyo <qkrwnsgk214@gmail.com>
2025-02-15 15:47:03 -0400

    componented about page

---
+++ frontend/src/components/About/AboutImage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="image-content">
+    <img 
+      src="@/assets/images/GALLERY/20200702_162018.jpg" 
+      alt="About Image"
+      class="about-image"
+    />
+  </div>
+</template>
+
+<style scoped>
+.about-image {
+  width: 100%;
+  max-width: 500px;
+  height: auto;
+}
+</style>
