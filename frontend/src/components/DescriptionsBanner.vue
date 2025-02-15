12e65c12b64901912af07766edbd1c3527db629e
juliayeon0906 <yeonyt0345@gmail.com>
2025-02-15 14:54:37 -0400

    frontend change reverted with gitignore

---
+++ frontend/src/components/DescriptionsBanner.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="description-container">
+        <div class="text-container">
+            <h2 class="desc-header">Our <span class="highlight">Value</span></h2>
+            <h5 class="description">At K-Beauty, customer satisfaction and exceptional service are our top priorities. Our dedicated team 
+            in Halifax is here to provide you with a welcoming and personalized experience.</h5>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+
+</script>
+
+<style scoped>
+    .description-container {
+        height: 50vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100vw;
+    }
+    .text-container {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+        flex-wrap: wrap;
+        gap: 50px;
+    }
+    .desc-header{
+        font-size: 64px;
+        text-shadow: 0.5px 0.5px rgba(0,0,0,0.7);
+        width: 209px;
+        line-height: 1;
+    }
+    .highlight {
+        color: #FFBF00;
+    }
+    .description {
+        font-family: 'Noto Sans', serif;
+        font-style: normal;
+        font-size: 18px;
+        font-optical-sizing: auto;
+        font-weight: 500;
+        font-variation-settings: "wdth" 100;
+        width: 494px;
+        line-height: 1.8;
+    }
+</style>
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <Banner />
+    <DescriptionsBanner />
+    <ServicesBanner/>
+    <CustomerExperiencesBanner/>
+</template>
+
+<script setup>
+import Banner from '@/components/Banner.vue';
+import CustomerExperiencesBanner from '@/components/CustomerExperiencesBanner.vue';
+import DescriptionsBanner from '@/components/DescriptionsBanner.vue';
+import ServicesBanner from '@/components/ServicesBanner.vue';
+</script>
+
+<style>
+    
+</style>
