0b24dbd88cf450360d68d49f0a6ea846199e7e49
Junha <qkrwnsgk214@gmail.com>
2025-06-19 20:54:06 +0900

    services bg / stylists bg change / ourservices clickable links

--- src/components/Home/ServicesBanner.vue
+++ src/components/Home/ServicesBanner.vue
@@ -1,13 +1,18 @@
 <template>
-    <div class="services-container">
-        <h2 class="service-header p-6 text-[48px] text-[#FFBF00] drop-shadow-[0.5px_0.5px_rgba(0,0,0,0.7)]">Our services</h2>
-        <div class="card-container">
-            <div v-for="(service, index) in services" :key="index" class="service-card">
-                <div class="service-bg" :style="{backgroundImage: `url(${service.img})`}"></div>
-                <h3 class="service-name">{{ service.name }}</h3>
-            </div>
-        </div>
+  <div class="services-container">
+    <h2 class="service-header p-6 text-[48px] text-[#FFBF00] drop-shadow-[0.5px_0.5px_rgba(0,0,0,0.7)]">Our services</h2>
+    <div class="card-container">
+      <router-link
+        v-for="(service, index) in services"
+        :key="index"
+        :to="'/services'"
+        class="service-card"
+      >
+        <div class="service-bg" :style="{ backgroundImage: `url(${service.img})` }"></div>
+        <h3 class="service-name">{{ service.name }}</h3>
+      </router-link>
     </div>
+  </div>
 </template>
 
 <script setup>
@@ -19,75 +24,75 @@ import img4 from '@/assets/images/services/Colour.jpg';
 import img5 from '@/assets/images/services/Styling.jpg';
 
 const services = computed(() => {
-    return [
-        {name: 'perm', img: img1},
-        {name: 'cut', img: img2},
-        {name: 'treatment', img: img3},
-        {name: 'colour', img: img4},
-        {name: 'styling', img: img5},
-    ]
+  return [
+    { name: 'perm', img: img1 },
+    { name: 'cut', img: img2 },
+    { name: 'treatment', img: img3 },
+    { name: 'colour', img: img4 },
+    { name: 'styling', img: img5 }
+  ];
 });
-
 </script>
 
 <style scoped>
-    .services-container {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        min-height: 60vh;
-        flex-direction: column;
-        padding: 20px;
-        width: 100%;
-        margin-bottom: 40px;
-    }
-    .card-container {
-        display: flex;
-        gap: 30px;
-        align-items: center;
-        justify-content: center;
-        width: 100%;
-    }
-    .service-card {
-        display: flex;
-        position: relative;
-        text-transform: uppercase;
-        justify-content: center;
-        align-items: center;
-        background-size: cover;
-        min-height: 50vh;
-        min-width: 15vw;
-        border-radius: 4.5px;
-        overflow: hidden;
-        transition: transform 0.5s ease;
-    }
-    .service-bg{
-        position: absolute;
-        width: 100%;
-        height: 100%;
-        background-size: cover;
-        background-position: center;
-        filter: brightness(50%);
-    }
-    .service-name{
-        position: relative;
-        color: #F2F2F2;
-        font-size: 20px;
-    }
-    .service-card:hover{
-        transform: scale(1.05);
-    }
-    @media only screen and (max-width: 1200px) {
-        .card-container {
-            flex-direction: column;
-            gap: 45px;
-        }
-        .service-card {
-            height: 360px;
-            width: 80%;
-        }
-        .service-header {
-            font-size: 48px;
-        }
-    }
+.services-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 60vh;
+  flex-direction: column;
+  padding: 20px;
+  width: 100%;
+  margin-bottom: 40px;
+}
+.card-container {
+  display: flex;
+  gap: 30px;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+}
+.service-card {
+  display: flex;
+  position: relative;
+  text-transform: uppercase;
+  justify-content: center;
+  align-items: center;
+  background-size: cover;
+  min-height: 50vh;
+  min-width: 15vw;
+  border-radius: 4.5px;
+  overflow: hidden;
+  transition: transform 0.5s ease;
+  text-decoration: none;
+}
+.service-bg {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-size: cover;
+  background-position: center;
+  filter: brightness(50%);
+}
+.service-name {
+  position: relative;
+  color: #f2f2f2;
+  font-size: 20px;
+}
+.service-card:hover {
+  transform: scale(1.05);
+}
+@media only screen and (max-width: 1200px) {
+  .card-container {
+    flex-direction: column;
+    gap: 45px;
+  }
+  .service-card {
+    height: 360px;
+    width: 80%;
+  }
+  .service-header {
+    font-size: 48px;
+  }
+}
 </style>
--- src/components/Services/ServiceMenu.vue
+++ src/components/Services/ServiceMenu.vue
@@ -1,70 +1,82 @@
 <template>
-    <div class="menu-container">
-        <div class="menu-item" v-for="{image, index} in MENU_OPTIONS" :key="index">
-            <img :src="image" alt="menu">
-        </div>
+  <div
+    class="menu-container"
+    :style="{ backgroundImage: `url(${menuBg})` }"
+  >
+    <div
+      class="menu-item"
+      v-for="{ image, index } in MENU_OPTIONS"
+      :key="index"
+    >
+      <img :src="image" alt="menu" />
     </div>
-
+  </div>
 </template>
+
 <script setup>
 import { MENU_OPTIONS } from '@/constants';
+import menuBg from '@/assets/images/Our_Services_Background_Image.jpg';
 </script>
 
 <style scoped>
 .menu-container {
-    display: flex;
-    width: 100%;
-    gap: 5%;
-    justify-content: center;
-    padding-right: 5%;
-    padding-left: 5%;
-    position: relative;
-    margin-top: 60vh;
-    padding-bottom: 20px;
+  display: flex;
+  width: 100%;
+  gap: 5%;
+  justify-content: center;
+  padding-right: 5%;
+  padding-left: 5%;
+  position: relative;
+  margin-top: 60vh;
+  padding-bottom: 20px;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
 }
+
 .menu-item {
-    background-color: #2B2B2B;
-    opacity: 0.95;
-    padding: 5% 3% 5%;
-    border-radius: 4px;
-    display: flex;
-    justify-content: center;
-    align-items: start;
-    transition: transform ease 0.5s;
+  background-color: #2b2b2b;
+  opacity: 0.95;
+  padding: 5% 3% 5%;
+  border-radius: 4px;
+  display: flex;
+  justify-content: center;
+  align-items: start;
+  transition: transform ease 0.5s;
 }
 
 .menu-item:hover {
-    transform: scale(1.05);
+  transform: scale(1.05);
 }
 
 @media only screen and (max-width: 1200px) {
-    .menu-container {
-        flex-direction: column;
-        gap: 15px;
-    }
-    .menu-item {
-        height: fit-content;
-    }
-    img {
-        width: 60%;
-    }
+  .menu-container {
+    flex-direction: column;
+    gap: 15px;
+  }
+  .menu-item {
+    height: fit-content;
+  }
+  img {
+    width: 60%;
+  }
 }
 
-@media only screen and (min-height: 1000px){
-    .menu-container{
-        margin-top: 40vh;
-    }
+@media only screen and (min-height: 1000px) {
+  .menu-container {
+    margin-top: 40vh;
+  }
 }
 
 @media only screen and (max-height: 850px) {
-    .menu-container{
-        margin-top: 70vh;
-    }
+  .menu-container {
+    margin-top: 70vh;
+  }
 }
 
 @media only screen and (max-height: 650px) {
-    .menu-container {
-        margin-top: 110vh;
-    }
+  .menu-container {
+    margin-top: 110vh;
+  }
 }
 </style>
